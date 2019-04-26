96ea14cba2a924a3fde667c71ad208f513c84fc1
Lý Đông Cảnh <lydongcanh@gmail.com>
2019-04-26 20:52:16 +0700

    Add base repository.

---
+++ Infrastructure/Data/KidClothesShopRepository.cs
@@ -0,0 +1,88 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using KidClothesShop.Core.Interfaces;
+using KidClothesShop.Core.Entities;
+
+namespace KidClothesShop.Infrastructure.Data
+{
+    public class KidClothesShopRepository<T> : IAsyncRepository<T> where T : BaseEntity
+    {
+        protected readonly KidClothesShopDBContext dbContext;
+
+        public KidClothesShopRepository(KidClothesShopDBContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public virtual async Task<T> GetByIdAsync(int id)
+        {
+            return await dbContext.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).ToListAsync();
+        }
+
+        public async Task<int> CountAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).CountAsync();
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            dbContext.Set<T>().Add(entity);
+            await dbContext.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            dbContext.Entry(entity).State = EntityState.Modified;
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            dbContext.Set<T>().Remove(entity);
+            await dbContext.SaveChangesAsync();
+        }
+
+        private IQueryable<T> ApplySpecification(ISpecification<T> specification)
+        {
+            var query = dbContext.Set<T>().AsQueryable();
+
+            // modify the IQueryable using the specification's criteria expression
+            if (specification.Criteria != null)
+                query = query.Where(specification.Criteria);
+
+            // Includes all expression-based includes
+            query = specification.Includes.Aggregate(query,
+                                    (current, include) => current.Include(include));
+
+            // Include any string-based include statements
+            query = specification.IncludeStrings.Aggregate(query,
+                                    (current, include) => current.Include(include));
+
+            // Apply ordering if expressions are set
+            if (specification.OrderBy != null)
+                query = query.OrderBy(specification.OrderBy);
+            else if (specification.OrderByDescending != null)
+                query = query.OrderByDescending(specification.OrderByDescending);
+
+            // Apply paging if enabled
+            if (specification.IsPagingEnabled)
+                query = query.Skip(specification.Skip).Take(specification.Take);
+
+            return query;
+        }
+    }
+}
