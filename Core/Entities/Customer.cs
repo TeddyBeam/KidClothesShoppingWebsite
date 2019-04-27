d7dd82868266d3fe44daca7ba1744e930d08f3fd
Lý Đông Cảnh <lydongcanh@gmail.com>
2019-04-27 21:33:35 +0700

    Add missing Customer entity.

---
+++ Core/Entities/Customer.cs
@@ -0,0 +1,17 @@
+﻿namespace KidClothesShop.Core.Entities
+{
+    public class Customer : BaseEntity
+    {
+        public string IdentityGuid { get; private set; }
+
+        private Customer()
+        {
+            // Required by EF
+        }
+
+        public Customer(string identity) : this()
+        {
+            IdentityGuid = identity;
+        }
+    }
+}
