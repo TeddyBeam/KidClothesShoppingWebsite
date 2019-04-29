999b656e2c3dfb68a115852036eb1bbde3865f14
Lý Đông Cảnh <lydongcanh@gmail.com>
2019-04-29 15:41:09 +0700

    Add Order service.

---
+++ Core/Interfaces/IOrderService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using KidClothesShop.Core.ValueObjects;
+
+namespace KidClothesShop.Core.Interfaces
+{
+    public interface IOrderService
+    {
+        Task CreateOrderAsync(int basketId, Address shippingAddress);
+    }
+}
---
+++ Core/Services/OrderService.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using KidClothesShop.Core.Interfaces;
+using KidClothesShop.Core.ValueObjects;
+using KidClothesShop.Core.Entities;
+
+namespace KidClothesShop.Core.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IAsyncRepository<Order> orderRepository;
+        private readonly IAsyncRepository<Basket> basketRepository;
+        private readonly IAsyncRepository<Product> productRepository;
+
+        public OrderService(IAsyncRepository<Basket> basketRepository,
+                            IAsyncRepository<Product> productRepository,
+                            IAsyncRepository<Order> orderRepository)
+        {
+            this.orderRepository = orderRepository;
+            this.basketRepository = basketRepository;
+            this.productRepository = productRepository;
+        }
+
+        public async Task CreateOrderAsync(int basketId, Address shippingAddress)
+        {
+            var basket = await basketRepository.SelectByIdAsync(basketId);
+            if (basket == null)
+                return; // TODO: Maybe throw an exception here?
+
+            var items = new List<OrderDetails>();
+            foreach (var item in basket.Details)
+            {
+                var product = await productRepository.SelectByIdAsync(item.ProductId);
+                var productOrdered = new ProductOrdered(product.Id, product.Name, product.PictureUri);
+                var orderDetails = new OrderDetails(productOrdered, item.UnitPrice, item.Quantity);
+                items.Add(orderDetails);
+            }
+            var order = new Order(basket.CustomerId, shippingAddress, items);
+
+            await orderRepository.AddAsync(order);
+        }
+    }
+}
