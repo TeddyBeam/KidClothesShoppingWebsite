412d547b10f3e70ea7e8f92a6d60a25b24df3e46
Lý Đông Cảnh <lydongcanh@gmail.com>
2019-04-29 18:15:21 +0700

    Add Constants class.

---
+++ Web/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace KidClothesShop.Web
+{
+    public static class Constants
+    {
+        public const string BASKET_COOKIENAME = "KID_CLOTHES_SHOP_KTPM12A_IUH_25";
+        public const int ITEMS_PER_PAGE = 10;
+    }
+}
